e28a71b3dba9a8541c992b6f2200b9ea2fb3e962
Libor Svejda <Libor.Svejda@unicorn.com>
2020-01-07 17:01:58 +0100

    ukladani uzivatelu do souboru, mixovani hesel

--- api-users.js
+++ api-users.js
@@ -1,6 +1,21 @@
 const dateFormat = require('dateformat');
+const fs = require('fs');
+const crypto = require('crypto');
+
+const FILE_USERS = "users.json";
+
+function zamixujHeslo(heslo) {
+    //let mix = heslo.split("").reverse().join("");
+    let mix = crypto.createHash("sha256").update(heslo).digest("hex");
+    mix = mix.split("").reverse().join("");
+    return mix;
+}
 
 let list = new Array();
+if (fs.existsSync(FILE_USERS)) {
+    list = JSON.parse(fs.readFileSync(FILE_USERS));
+}
+console.log(list);
 
 exports.apiUsers = function (req, res, obj) {
     if (req.pathname.endsWith("/list")) {
@@ -10,7 +25,8 @@ exports.apiUsers = function (req, res, obj) {
         let dt = new Date();
         addObj.time = dateFormat(dt, "HH.MM.ss");
         addObj.login = req.parameters.login;
-        addObj.password = req.parameters.password;
+        addObj.password = zamixujHeslo(req.parameters.password);
         list.push(addObj);
+        fs.writeFileSync(FILE_USERS, JSON.stringify(list));
     }
 }
---
+++ test.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+function zamixujHeslo(heslo) {
+    //let mix = heslo.split("").reverse().join("");
+    let mix = crypto.createHash("sha256").update(heslo).digest("hex");
+    mix = mix.split("").reverse().join("");
+    return mix;
+}
+
+//registrace
+let hesloZRegistrace = "testicek";
+let mixZRegistrace = zamixujHeslo(hesloZRegistrace);
+console.log("mixZRegistrace: "+mixZRegistrace); //toto bude ulozeno v souboru/databazi
+
+//prihlaseni
+let hesloZPrihlaseni = "testicek";
+let mixZPrihlaseni = zamixujHeslo(hesloZPrihlaseni);
+console.log("mixZPrihlaseni: "+mixZPrihlaseni);
+if (mixZPrihlaseni === mixZRegistrace) {
+    console.log("OK");
+} else {
+    console.error("spatne heslo!");
+}
